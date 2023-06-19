73d05b2c08c0a0e7582c37e0801a7bc868279008
Radu Mihnea <rmihneaadrian@yahoo.com>
2023-06-19 15:53:58 +0300

    Release 2.1 (#33)
    * Release Candidate 2.0 (#28)
    
    * feat(events): routes related to events
    
    * style: linters
    
    * feat(events): added update & destroy routed for events
    
    * test(events): added deletion feature tests
    
    * style: linters
    
    * fix(slugs): fixes to slugs
    
    * fix(test): fixes to test
    
    * fix(enrollments): added everything realated to enrollments
    
    * style: linters
    
    * style: linters
    
    * test: fix failing tests
    
    * test: fix failing tests
    
    * feat(badges): add badges
    
    * feat(badges): add badges
    
    * feat(badges): add badges
    
    * Release Candidate 2.1 (#32)
    
    * feat(events): routes related to events
    
    * style: linters
    
    * feat(events): added update & destroy routed for events
    
    * test(events): added deletion feature tests
    
    * style: linters
    
    * fix(slugs): fixes to slugs
    
    * fix(test): fixes to test
    
    * fix(enrollments): added everything realated to enrollments
    
    * style: linters
    
    * style: linters
    
    * test: fix failing tests
    
    * test: fix failing tests
    
    * feat(badges): add badges
    
    * feat(badges): add badges
    
    * feat(badges): add badges
    
    * feat(events): add event location data (#30)
    
    * CPH-54 Dummy data (#31)
    
    * feat(events): add event location data
    
    * feat(data): add dummy data
    
    * style: linters
    
    * fix(migrations): update summary column to fit encrypted data

--- app/Console/Commands/EnsureDatabaseStateIsLoaded.php
+++ app/Console/Commands/EnsureDatabaseStateIsLoaded.php
@@ -5,7 +5,12 @@ namespace App\Console\Commands;
 use Database\State\EnsureBadgesArePresent;
 use Database\State\EnsureCitiesArePresent;
 use Database\State\EnsureCountriesArePresent;
+use Database\State\EnsureEnrollmentsArePresent;
+use Database\State\EnsureEventMappingsArePresent;
+use Database\State\EnsureEventsArePresent;
 use Database\State\EnsureEventTypesArePresent;
+use Database\State\EnsureOrganizationsArePresent;
+use Database\State\EnsureVolunteersArePresent;
 use Illuminate\Console\Command;
 
 class EnsureDatabaseStateIsLoaded extends Command
@@ -21,6 +26,11 @@ class EnsureDatabaseStateIsLoaded extends Command
             new EnsureCitiesArePresent(),
             new EnsureEventTypesArePresent(),
             new EnsureBadgesArePresent(),
+            new EnsureOrganizationsArePresent(),
+            new EnsureEventsArePresent(),
+            new EnsureEventMappingsArePresent(),
+            new EnsureVolunteersArePresent(),
+            new EnsureEnrollmentsArePresent(),
         ])->each->__invoke();
     }
 }
