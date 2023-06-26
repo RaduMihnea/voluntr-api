474983765e5e0f1fba035cf8982d45d1aeb769f6
radumihnea <rmihneaadrian@yahoo.com>
2023-06-27 01:55:03 +0300

    feat(notifications): added first couple of mails

---
+++ lang/en/mails.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'footer_salutations' => 'With much love,',
+    'footer_signature' => 'The Voluntr. team',
+    'new_enrollment' => [
+        'subject' => 'A new volunteer enrolled for your event!',
+        'greetings' => 'Hello!',
+        'paragraph1' => 'You have a new enrollment for the event :event.',
+        'paragraph2' => 'You can see the details of the event by clicking the button below.',
+        'button' => 'See event',
+    ],
+    'enrollment_state_changed' => [
+        'approved' => [
+            'subject' => 'Your enrollment has been approved!',
+            'greetings' => 'Hello :name!',
+            'paragraph1' => 'Your enrollment for the event :event has been approved.',
+            'paragraph2' => 'The organization will contact you soon, be sure to check your email and stick close to your phone.',
+        ],
+        'rejected' => [
+            'subject' => 'Your enrollment has been rejected!',
+            'greetings' => 'Hello :name!',
+            'paragraph1' => 'Unfortunately, your enrollment for the event :event has been rejected.',
+            'paragraph2' => "This wasn't a match, but don't worry, there are plenty of other events you can enroll for. Check them out by clicking the link below",
+            'button' => 'See events',
+        ],
+    ],
+];
