e6b38822c0a2578d45934f16e8b9cfff4664248c
Radu Mihnea <rmihneaadrian@yahoo.com>
2023-06-27 01:58:15 +0300

    feat(auth): reset and forgot password (#40)

--- src/Domains/Volunteer/DTOs/VolunteerPublicProfileData.php
+++ src/Domains/Volunteer/DTOs/VolunteerPublicProfileData.php
@@ -4,17 +4,14 @@ namespace Domain\Volunteer\DTOs;
 
 use DB;
 use Domain\Badges\DTOs\BadgeData;
-use Domain\Badges\Models\Badge;
 use Domain\Events\DTOs\VolunteerEnrollmentData;
 use Domain\Events\Enums\EnrollmentStateEnum;
 use Domain\Volunteer\Models\Volunteer;
-use Illuminate\Support\Facades\Hash;
 use Spatie\LaravelData\Attributes\DataCollectionOf;
 use Spatie\LaravelData\Attributes\MapName;
 use Spatie\LaravelData\Data;
 use Spatie\LaravelData\DataCollection;
 use Spatie\LaravelData\Mappers\SnakeCaseMapper;
-use Support\Actions\CreateUniqueSlugAction;
 
 #[MapName(SnakeCaseMapper::class)]
 class VolunteerPublicProfileData extends Data
